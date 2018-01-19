4ba92395ad1a871f0cb67efeb29b1046e51f7788
tiagolpadua <tiagolpadua@gmail.com>
2018-01-19 15:38:32 -0200

    ex 6.6

--- src/main/java/br/com/caelum/ingresso/controller/CompraController.java
+++ src/main/java/br/com/caelum/ingresso/controller/CompraController.java
@@ -2,6 +2,7 @@ package br.com.caelum.ingresso.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +24,13 @@ public class CompraController {
 	public ModelAndView enviarParaPagamento(CarrinhoForm carrinhoForm) {
 		ModelAndView modelAndView = new ModelAndView("redirect:/compra");
 		carrinhoForm.toIngressos(sessaoDao, lugarDao).forEach(carrinho::add);
+		return modelAndView;
+	}
+
+	@GetMapping("/compra")
+	public ModelAndView checkout() {
+		ModelAndView modelAndView = new ModelAndView("compra/pagamento");
+		modelAndView.addObject("carrinho", carrinho);
 		return modelAndView;
 	}
 }
--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,5 +27,10 @@ public class Carrinho {
 	public boolean isSelecionado(Lugar lugar) {
 		return ingressos.stream().map(Ingresso::getLugar)
 				.anyMatch(l -> l.equals(lugar));
+	}
+
+	public BigDecimal getTotal() {
+		return ingressos.stream().map(Ingresso::getPreco)
+				.reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
 	}
 }
