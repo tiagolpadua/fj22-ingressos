b9b5be5091abbfa5501072ce28a9b931b0d7d5fc
tiagolpadua <tiagolpadua@gmail.com>
2018-01-19 15:34:28 -0200

    ex 6.4

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -22,4 +22,9 @@ public class Carrinho {
 	public void setIngressos(List<Ingresso> ingressos) {
 		this.ingressos = ingressos;
 	}
+
+	public boolean isSelecionado(Lugar lugar) {
+		return ingressos.stream().map(Ingresso::getLugar)
+				.anyMatch(l -> l.equals(lugar));
+	}
 }
