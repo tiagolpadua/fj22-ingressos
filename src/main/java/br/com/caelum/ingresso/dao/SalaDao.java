864fbabb4b56f3cfc2d5557693b03ec5e5c80892
tiagolpadua <tiagolpadua@gmail.com>
2018-01-18 22:34:26 -0200

    ex 5.2

--- src/main/java/br/com/caelum/ingresso/dao/SalaDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SalaDao.java
@@ -1,11 +1,14 @@
 package br.com.caelum.ingresso.dao;
 
-import br.com.caelum.ingresso.model.Sala;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
 
 /**
  * Created by nando on 03/03/17.
@@ -13,23 +16,23 @@ import java.util.List;
 @Repository
 public class SalaDao {
 
-    @PersistenceContext
-    private EntityManager manager;
+	@PersistenceContext
+	private EntityManager manager;
 
-    public Sala findOne(Integer id) {
+	public Sala findOne(Integer id) {
+		return manager.find(Sala.class, id);
+	}
 
-        return manager.find(Sala.class, id);
-    }
+	public void save(Sala sala) {
+		manager.merge(sala);
+	}
 
-    public void save(Sala sala) {
-        manager.merge(sala);
-    }
+	public List<Sala> findAll() {
+		return manager.createQuery("select s from Sala s", Sala.class)
+				.getResultList();
+	}
 
-    public List<Sala> findAll() {
-        return manager.createQuery("select s from Sala s", Sala.class).getResultList();
-    }
-
-    public void delete(Integer id) {
-        manager.remove(findOne(id));
-    }
+	public void delete(Integer id) {
+		manager.remove(findOne(id));
+	}
 }
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,6 +3,8 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -80,5 +82,9 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
 	}
 }
