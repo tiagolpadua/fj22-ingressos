5c161666dce16a1d049f7e60b5283d47cad852f4
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2021-01-07 21:43:29 -0300

    ex2.12

--- src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
+++ src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
@@ -1,14 +1,10 @@
 package br.com.caelum.ingresso.validacao;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
-import br.com.caelum.ingresso.model.Filme;
-import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
 public class GerenciadorDeSessao {
